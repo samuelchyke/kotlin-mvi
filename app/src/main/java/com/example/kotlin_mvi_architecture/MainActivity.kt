9e6c2d15d442cb7a8687ac4b1038b2394747a3a2
CyloTM <samuelchyke@hotmail.com>
2021-05-17 16:35:46 +0000

    Main Fragment

---
+++ app/src/main/java/com/example/kotlin_mvi_architecture/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.kotlin_mvi_architecture
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity()
+{
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        showMainFragment()
+
+    }
+    
+    private fun showMainFragment(){
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, MainFragment(), "MainFragment")
+            .commit()
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/example/kotlin_mvi_architecture/MainFragment.kt
@@ -0,0 +1,49 @@
+package com.example.kotlin_mvi_architecture
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.*
+import androidx.fragment.app.Fragment
+import com.example.kotlin_mvi_architecture.R
+
+
+class MainFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.main_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return super.onOptionsItemSelected(item)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
